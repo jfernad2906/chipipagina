fe6234e24ddf6dc0ef0f11b943107fabd3279861
jfernad2906 <jfernad2906@g.educaand.es>
2025-02-03 12:05:22 +0100

    Create numeros.js

---
+++ numeros.js
@@ -0,0 +1,7 @@
+    var numero = prompt ("Escribe un numero del 1 al 100"); 
+            var i = 1;
+            var suma = 0;
+            
+            if (numero<1 || numero >100) { 
+                        document.write ("este numero NO nos vale");
+                      }
