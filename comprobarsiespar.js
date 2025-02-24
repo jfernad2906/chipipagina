e8dd410bed9c608eeb70d69cd7e9a8c9baa44e69
jfernad2906 <jfernad2906@g.educaand.es>
2025-02-24 12:09:54 +0100

    Create comprobarsiespar.js

---
+++ comprobarsiespar.js
@@ -0,0 +1,16 @@
+function comprobarsiespar (numero)
+  {
+    var esPar;
+    var i;
+    
+        if (numero % i ===0)
+        {
+          esPar = true;
+          return esPar;
+        }
+ 
+       else   {
+              esPar = false;
+              return esPar;
+              }
+  }
